b40684a3011f254f730066e4ec370ecd8921369e
Md. Saiful <saifulpro247@gmail.com>
2023-06-14 20:21:32 +0600

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,20 @@
     margin: 30,
     loop: true,
     dots: true,
+    responsiveClass: true,
+    responsive:{
+      0:{
+        items: 1,
+        dots: false,
+      },
+      767:{
+        items: 2
+      },
+      1200:{
+        items: 3
+      }
+
+    }
   })
 
     
