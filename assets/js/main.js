d5903657c3d3e7de0e8d6af695159636d33d0132
Md. Saiful <saifulpro247@gmail.com>
2023-06-15 17:44:34 +0600

    this

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 (function($) {
     "use strict";
 
+$('#mobile-menu').meanmenu({
+      meanScreenWidth: "767",
+      meanMenuContainer: '.responsive-menu'
+    });
 
+$(".flexnav").flexNav({});
+// Menu js for Position fixed
+    $(window).scroll(function(){
+      var window_top = $(window).scrollTop() + 1; 
+      if (window_top > 200) {
+        $('.header_menu_area').addClass('menu_fixed animated fadeInDown');
+      } else {
+        $('.header_menu_area').removeClass('menu_fixed animated fadeInDown');
+      }
+    }); 
+
+
+// testimonial slider
   $('.single_slider_item').owlCarousel({
     items: 3,
     margin: 30,
