0f373e6c09673b8e28431b82d4db7f886d0bf0c5
Md. Saiful <saifulpro247@gmail.com>
2023-06-20 22:41:22 +0600

    some changes

---
+++ header.php
@@ -0,0 +1,55 @@
+<?php 
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <link rel="stylesheet" href="assets/fonts/stylesheet.css">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/aos.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/fonts/icofont.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <title> Document </title>                     
+    </head>
+    <body>
+        
+        <header class="main-header incafe_header_area">
+            <div class="header_menu_area">
+                <div class="container">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-md-3 col-lg-3 col-sm-6">
+                            <div class="site_logo">
+                                <a href="#"><img src="assets/img/logo.webp" alt="" class="img-fluid"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-9 col-lg-6 d-none d-md-block">
+                            <div class="main_menu_area  d-flex justify-content-lg-end">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Service</a></li>
+                                    <li><a href="#">Works</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="#" class="hiring_button">Hiring</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 d-none d-lg-block">
+                            <div class="header_right_area d-flex justify-content-end">
+                                <ul>
+                                    <li><a href="#" class="project_start_btn">Start a Project</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- this is mobile menu code -->
+
+            </div>
+        </header>
