7829e76fcbcd6b66c135fda736634eff1b95d90d
Asifur Rahman <asifur94@outlook.com>
2023-07-12 02:24:11 +0600

    this

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,7 @@
     loop: true,
     dots: true,
     responsiveClass: true,
+    autoplay: true,
     responsive: {
       0: {
         items: 1,
@@ -38,6 +39,28 @@
     loop: true,
     dots: true,
     responsiveClass: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      767: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".related_slider").owlCarousel({
+    items: 4,
+    margin: 30,
+    loop: true,
+    dots: true,
+    responsiveClass: true,
+    autoplay: true,
     responsive: {
       0: {
         items: 1,
